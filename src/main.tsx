9ae8d4d7035187f869c8d5e45a8e88469b7882e5
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-05-02 18:36:26 +0800

    v3.1 Parallax Fix & Router Config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import Landing from './pages/Landing';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Landing />
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Landing />}></Route>
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
 );
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,34 +1,35 @@
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { useEffect, useState } from 'react';
 import LandingNav from '../components/LandingNav';
 import '../main.scss';
 
 function Landing() {
+	const [offsetY, setOffset] = useState(0);
+	const handleScroll = () => setOffset(window.pageYOffset);
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
 	return (
 		<div className='landing'>
 			<div className='landing-header'>
 				<div className='landing-header-col1'>
 					<h1>For better unity, help your community.</h1>
-					<button>Find your next event</button>
+					<button onClick={() => console.log('clicked')}>
+						Find your next event
+					</button>
 				</div>
 				<div className='landing-header-col2'>
 					<img src='/assets/landing2-backdrop.png' alt='error' />
 				</div>
 			</div>
-			<Parallax pages={2} style={{ top: '0', left: '0' }}>
-				<ParallaxLayer sticky={{ start: 0, end: 5 }}>
-					<LandingNav />
-				</ParallaxLayer>
-				<ParallaxLayer
-					offset={1}
-					style={{
-						width: '100vw',
-						backgroundColor: 'white',
-						boxShadow: '0 -3px 10px 5px rgba(0, 0, 0, 0.25)',
-					}}
-					className='landing-body'>
-					<h1>Events</h1>
-				</ParallaxLayer>
-			</Parallax>
+			<div className='landing-body'>
+				<h1>Events</h1>
+			</div>
+			<div className='landing-navbar'>
+				<LandingNav />
+			</div>
 		</div>
 	);
 }
