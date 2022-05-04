b33c9e9943ba9477e9490d47d0fdb52c20818674
ZenShi <velezrenz@gmail.com>
2022-05-04 16:22:39 +0800

    v5.4 Signup Updates

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -9,53 +9,71 @@ function Signup() {
 			<div className='signup-navbar'>
 				<AuthNav />
 			</div>
-			<div className='signup-body-container'>
-				<div className='signup-body-container-header'>
-					<h1 className='signup-body-container-header-text1'>Sign Up</h1>
-					<h1 className='signup-body-container-header-text2'>
-						Register with your valid email address.
-					</h1>
-				</div>
-				<div className='signup-body-container-fullname'>
-					<h1 className='signup-body-container-fullname-text'>First Name</h1>
-					<input
-						className='signup-body-container-fullname-input'
-						type='text'></input>
-					<h1 className='signup-body-container-fullname-text'>Last Name</h1>
-					<input
-						className='signup-body-container-fullname-input'
-						type='text'></input>
-				</div>
-				<div className='signup-body-container-email'>
-					<h1 className='signup-body-container-email-text'>Email</h1>
-					<input
-						className='signup-body-container-email-input'
-						type='text'></input>
-				</div>
-				<div className='signup-body-container-password'>
-					<h1 className='signup-body-container-password-text'> Password</h1>
-					<input
-						className='signup-body-container-password-input'
-						type='text'></input>
-					<h1 className='signup-body-container-password-text'>
-						Confirm Password
-					</h1>
-					<input
-						className='signup-body-container-password-input'
-						type='text'></input>
-				</div>
-				<div className='signup-body-container-button'>
-					<button className='signup-body-container-button-create'>
-						Create Account
-					</button>
-				</div>
-				<div className='signup-body-container-footer'>
-					<h1 className='signup-body-container-footer-text'>
-						Already have an account?
-					</h1>
-					<Link className='signup-body-container-footer-login' to={'/login'}>
-						Login
-					</Link>
+			<div className='signup-body'>
+				<div className='signup-body-container'>
+					<div className='signup-body-container-header'>
+						<h1 className='signup-body-container-header-text1'>Sign Up</h1>
+						<h1 className='signup-body-container-header-text2'>
+							Register with your valid email address.
+						</h1>
+					</div>
+					<div className='signup-body-container-fullname'>
+						<div className='signup-body-container-fullname-col1'>
+							<div className='signup-body-container-fullname-col1-label'>
+								<h1 className='signup-body-container-fullname-col1-label-text'>
+									First Name
+								</h1>
+							</div>
+							<div className='signup-body-container-fullname-col1-input'>
+								<input
+									className='signup-body-container-fullname-col1-input-field'
+									type='text'></input>
+							</div>
+						</div>
+						<div className='signup-body-container-fullname-col2'>
+							<div className='signup-body-container-fullname-col2-label'>
+								<h1 className='signup-body-container-fullname-col2-label-text'>
+									Last Name
+								</h1>
+							</div>
+							<div className='signup-body-container-fullname-col2-input'>
+								<input
+									className='signup-body-container-fullname-col2-input-field'
+									type='text'></input>
+							</div>
+						</div>
+					</div>
+					<div className='signup-body-container-email'>
+						<h1 className='signup-body-container-email-text'>Email</h1>
+						<input
+							className='signup-body-container-email-input'
+							type='text'></input>
+					</div>
+					<div className='signup-body-container-password'>
+						<h1 className='signup-body-container-password-text'> Password</h1>
+						<input
+							className='signup-body-container-password-input'
+							type='text'></input>
+						<h1 className='signup-body-container-password-text'>
+							Confirm Password
+						</h1>
+						<input
+							className='signup-body-container-password-input'
+							type='text'></input>
+					</div>
+					<div className='signup-body-container-button'>
+						<button className='signup-body-container-button-create'>
+							Create Account
+						</button>
+					</div>
+					<div className='signup-body-container-footer'>
+						<h1 className='signup-body-container-footer-text'>
+							Already have an account?
+						</h1>
+						<Link className='signup-body-container-footer-login' to={'/login'}>
+							Login
+						</Link>
+					</div>
 				</div>
 			</div>
 			<div>
