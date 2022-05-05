72f34c4841c73d1a2b7f73a9ef01abe8bd695c91
Mervin John C. Tampus <79685609+jdesolate@users.noreply.github.com>
2022-05-05 20:58:41 +0800

    v6.3 Create UserData DB

--- src/firebase/User.tsx
+++ src/firebase/User.tsx
@@ -1,33 +1,65 @@
 class User {
-    name: string;
-    avatar: any;
-    rank: string;
-    expTotal: Int32Array;
-    constructor (name:string, avatar:any, rank:string, expTotal:Int32Array) {
-        this.name = name;
-        this.avatar = avatar;
-        this.rank = rank;
-        this.expTotal = expTotal;
-    }
-    toString() {
-        return this.name + ', ' + this.avatar + ', ' + this.rank + ', ' + this.expTotal;
-    }
+	name: any;
+	avatar: any;
+	rank: string;
+	expTotal: number;
+	eventsJoined: Int32Array;
+	badgesCollected: Int32Array;
+	constructor(
+		name: string,
+		avatar: any,
+		rank: string,
+		expTotal: number,
+		eventsJoined: Int32Array,
+		badgesCollected: Int32Array
+	) {
+		this.name = name;
+		this.avatar = avatar;
+		this.rank = rank;
+		this.expTotal = expTotal;
+		this.eventsJoined = eventsJoined;
+		this.badgesCollected = badgesCollected;
+	}
+	toString() {
+		return (
+			this.name +
+			', ' +
+			this.avatar +
+			', ' +
+			this.rank +
+			', ' +
+			this.expTotal +
+			', ' +
+			this.badgesCollected +
+			', ' +
+			this.eventsJoined
+		);
+	}
 }
 
 // Firestore data converter
 const userConverter = {
-    toFirestore: (Indiv:User) => {
-        return {
-            name: Indiv.name,
-            avatar: Indiv.avatar,
-            rank: Indiv.rank,
-            expTotal: Indiv.expTotal
-            };
-    },
-    fromFirestore: (snapshot:any, options:any) => {
-        const data = snapshot.data(options);
-        return new User(data.name, data.avatar, data.rank, data.expTotal);
-    }
+	toFirestore: (Indiv: User) => {
+		return {
+			name: Indiv.name,
+			avatar: Indiv.avatar,
+			rank: Indiv.rank,
+			expTotal: Indiv.expTotal,
+			eventsJoined: Indiv.eventsJoined,
+			badgesCollected: Indiv.badgesCollected,
+		};
+	},
+	fromFirestore: (snapshot: any, options: any) => {
+		const data = snapshot.data(options);
+		return new User(
+			data.name,
+			data.avatar,
+			data.rank,
+			data.expTotal,
+			data.badgesCollected,
+			data.eventsJoined
+		);
+	},
 };
 
 export default userConverter;
