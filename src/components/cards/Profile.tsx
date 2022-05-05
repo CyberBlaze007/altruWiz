923845ef97c8f0d137b6f2c1bd21701195651612
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-05-05 22:46:34 +0800

    v6.5 Dashboard Profile Updates

--- src/components/cards/Profile.tsx
+++ src/components/cards/Profile.tsx
@@ -23,136 +23,179 @@ function Profile() {
 					<div className='profile-body-sec1-form'>
 						<div className='profile-body-sec1-form-fname'>
 							<h1 className='profile-body-sec1-form-label'>First Name</h1>
-							<TextField
-								variant={editState ? 'standard' : 'outlined'}
-								color='secondary'
-								size='small'
-								className='profile-body-sec1-form-fname-field'
-								margin='dense'
-								value={firstName}
-								disabled={editState}
-								onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-									setFirstName(event.target.value)
-								}
-								fullWidth
-							/>
+							{editState ? (
+								<h1 className='profile-body-sec1-form-data'>{firstName}</h1>
+							) : null}
+							{!editState ? (
+								<TextField
+									variant={editState ? 'standard' : 'outlined'}
+									color='secondary'
+									size='small'
+									className='profile-body-sec1-form-fname-field'
+									margin='dense'
+									value={firstName}
+									disabled={editState}
+									onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+										setFirstName(event.target.value)
+									}
+									fullWidth
+								/>
+							) : null}
 						</div>
 						<div className='profile-body-sec1-form-lname'>
 							<h1 className='profile-body-sec1-form-label'>Last Name</h1>
-							<TextField
-								variant={editState ? 'standard' : 'outlined'}
-								color='secondary'
-								size='small'
-								className='profile-body-sec1-form-lname-field'
-								margin='dense'
-								value={lastName}
-								disabled={editState}
-								onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-									setLastName(event.target.value)
-								}
-								fullWidth
-							/>
+							{editState ? (
+								<h1 className='profile-body-sec1-form-data'>{lastName}</h1>
+							) : null}
+							{!editState ? (
+								<TextField
+									variant={editState ? 'standard' : 'outlined'}
+									color='secondary'
+									size='small'
+									className='profile-body-sec1-form-lname-field'
+									margin='dense'
+									value={lastName}
+									disabled={editState}
+									onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+										setLastName(event.target.value)
+									}
+									fullWidth
+								/>
+							) : null}
 						</div>
 						<div className='profile-body-sec1-form-gender'>
 							<h1 className='profile-body-sec1-form-label'>Gender</h1>
-							<TextField
-								variant={editState ? 'standard' : 'outlined'}
-								color='secondary'
-								sx={{ minWidth: '8rem' }}
-								size='small'
-								className='profile-body-sec1-form-gender-field'
-								margin='dense'
-								value={gender}
-								disabled={editState}
-								select
-								onChange={(event) => setGender(event.target.value)}>
-								<MenuItem value={'Male'}>Male</MenuItem>
-								<MenuItem value={'Female'}>Female</MenuItem>
-								<MenuItem value={'Prefer not to say'}>
-									Prefer not to say
-								</MenuItem>
-							</TextField>
+							{editState ? (
+								<h1 className='profile-body-sec1-form-data'>{gender}</h1>
+							) : null}
+							{!editState ? (
+								<TextField
+									variant={editState ? 'standard' : 'outlined'}
+									color='secondary'
+									sx={{ minWidth: '8rem' }}
+									size='small'
+									className='profile-body-sec1-form-gender-field'
+									margin='dense'
+									value={gender}
+									disabled={editState}
+									select={!editState}
+									onChange={(event) => setGender(event.target.value)}>
+									<MenuItem value={'Male'}>Male</MenuItem>
+									<MenuItem value={'Female'}>Female</MenuItem>
+									<MenuItem value={'Prefer not to say'}>
+										Prefer not to say
+									</MenuItem>
+								</TextField>
+							) : null}
 						</div>
 						<div className='profile-body-sec1-form-bday'>
 							<h1 className='profile-body-sec1-form-label'>Birthday</h1>
-							<TextField
-								variant={editState ? 'standard' : 'outlined'}
-								color='secondary'
-								size='small'
-								className='profile-body-sec1-form-bday-field'
-								type='date'
-								margin='dense'
-								value={bday}
-								disabled={editState}
-								onChange={(event) => setBday(event.target.value)}
-							/>
+							{editState ? (
+								<h1 className='profile-body-sec1-form-data'>{bday}</h1>
+							) : null}
+							{!editState ? (
+								<TextField
+									variant={editState ? 'standard' : 'outlined'}
+									color='secondary'
+									size='small'
+									className='profile-body-sec1-form-bday-field'
+									type={editState ? 'text' : 'date'}
+									margin='dense'
+									value={bday}
+									disabled={editState}
+									onChange={(event) => setBday(event.target.value)}
+								/>
+							) : null}
 						</div>
 						<div className='profile-body-sec1-form-email'>
 							<h1 className='profile-body-sec1-form-label'>Email</h1>
-							<TextField
-								variant={editState ? 'standard' : 'outlined'}
-								color='secondary'
-								size='small'
-								className='profile-body-sec1-form-email-field'
-								margin='dense'
-								value={email}
-								disabled={editState}
-								onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-									setEmail(event.target.value)
-								}
-								fullWidth
-							/>
+							{editState ? (
+								<h1 className='profile-body-sec1-form-data'>{email}</h1>
+							) : null}
+							{!editState ? (
+								<TextField
+									variant={editState ? 'standard' : 'outlined'}
+									color='secondary'
+									size='small'
+									className='profile-body-sec1-form-email-field'
+									margin='dense'
+									value={email}
+									disabled={editState}
+									onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+										setEmail(event.target.value)
+									}
+									fullWidth
+								/>
+							) : null}
 						</div>
 						<div className='profile-body-sec1-form-address'>
 							<h1 className='profile-body-sec1-form-label'>Address</h1>
-							<TextField
-								variant={editState ? 'standard' : 'outlined'}
-								color='secondary'
-								size='small'
-								className='profile-body-sec1-form-address-field'
-								margin='dense'
-								value={address}
-								disabled={editState}
-								onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-									setAddress(event.target.value)
-								}
-								fullWidth
-							/>
+							{editState ? (
+								<h1 className='profile-body-sec1-form-data'>{address}</h1>
+							) : null}
+							{!editState ? (
+								<TextField
+									variant={editState ? 'standard' : 'outlined'}
+									color='secondary'
+									size='small'
+									className='profile-body-sec1-form-address-field'
+									margin='dense'
+									value={address}
+									disabled={editState}
+									onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+										setAddress(event.target.value)
+									}
+									fullWidth
+								/>
+							) : null}
 						</div>
 					</div>
 					<div className='profile-body-sec1-footer'>
 						<button
-							className='profile-body-sec1-footer-button'
+							className={
+								editState
+									? 'profile-body-sec1-footer-button'
+									: 'profile-body-sec1-footer-button-hidden'
+							}
 							onClick={() => setEditState(false)}>
 							Edit
 						</button>
 					</div>
 				</div>
-				<div className='profile-body-divider'>
+				{/* <div className='profile-body-divider'>
 					<AddBoxOutlinedIcon />
-				</div>
+				</div> */}
 				<div className='profile-body-sec2'>
 					<div className='profile-body-sec2-title'>
 						<h1>About You</h1>
 					</div>
 					<div className='profile-body-sec2-form'>
-						<TextField
-							variant={editState ? 'standard' : 'outlined'}
-							color='secondary'
-							placeholder='Write something about you...'
-							size='small'
-							multiline
-							rows={5}
-							className='profile-body-sec2-form-field'
-							value={description}
-							disabled={editState}
-							onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-								setDescription(event.target.value)
-							}
-							fullWidth
-						/>
+						{editState ? (
+							<p className='profile-body-sec2-form-data'>{description}</p>
+						) : null}
+						{!editState ? (
+							<TextField
+								variant={editState ? 'standard' : 'outlined'}
+								color='secondary'
+								placeholder='Write something about you...'
+								size='small'
+								multiline
+								rows={5}
+								className='profile-body-sec2-form-field'
+								value={description}
+								disabled={editState}
+								onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+									setDescription(event.target.value)
+								}
+								fullWidth
+							/>
+						) : null}
 						<button
-							className='profile-body-sec2-form-button'
+							className={
+								editState
+									? 'profile-body-sec2-form-button-hidden'
+									: 'profile-body-sec2-form-button'
+							}
 							onClick={() => setEditState(true)}>
 							Save Changes
 						</button>
