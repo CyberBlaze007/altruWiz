719259f00da9ffc0dd688d006eeff66bcd40c51f
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-05-13 17:09:09 +0800

    v8.12 Badges Update

--- src/components/cards/Badges.tsx
+++ src/components/cards/Badges.tsx
@@ -1,7 +1,37 @@
+import { ConstructionOutlined } from '@mui/icons-material';
 import React from 'react';
+import { badges } from '../../pseudodata/badges';
 
 function Badges() {
-	return <div>Badges</div>;
+	return (
+		<div className='badges'>
+			<div className='badges-list'>
+				{badges.map((data) => (
+					<div key={data.id} className='badges-list-card'>
+						<div className='badges-list-card-overlay'>
+							<img src={data.image} />
+							<div className='badges-list-card-overlay-details'>
+								<h1 className='badges-list-card-overlay-details-name'>
+									{data.name}
+								</h1>
+								<h1 className='badges-list-card-overlay-details-desc'>
+									{data.desc}
+								</h1>
+								<div className='badges-list-card-overlay-details-date'>
+									<h1 className='badges-list-card-overlay-details-date-text1'>
+										{data.date}
+									</h1>
+									<h1 className='badges-list-card-overlay-details-date-text2'>
+										DATE ACQUIRED
+									</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Badges;
---
+++ src/pseudodata/badges.tsx
@@ -0,0 +1,37 @@
+export const badges = [
+	{
+		id: 1,
+		image: '/src/pseudodata/images/badge.png',
+		name: 'Infant Steps',
+		desc: 'This user has completed 5 tasks.',
+		date: '23-Feb-2022',
+	},
+	{
+		id: 2,
+		image: '/src/pseudodata/images/badge.png',
+		name: 'Toddler Steps',
+		desc: 'This user has completed 5 tasks.',
+		date: '24-Feb-2022',
+	},
+	{
+		id: 3,
+		image: '/src/pseudodata/images/badge.png',
+		name: 'Preschool Steps',
+		desc: 'This user has completed 5 tasks.',
+		date: '25-Feb-2022',
+	},
+	{
+		id: 4,
+		image: '/src/pseudodata/images/badge.png',
+		name: 'Adolescent Steps',
+		desc: 'This user has completed 5 tasks.',
+		date: '26-Feb-2022',
+	},
+	{
+		id: 5,
+		image: '/src/pseudodata/images/badge.png',
+		name: 'Adult Steps',
+		desc: 'This user has completed 5 tasks.',
+		date: '27-Feb-2022',
+	},
+];
