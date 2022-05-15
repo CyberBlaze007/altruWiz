4c680b63a2f9934a841aeccd6abbca3ad2e184cc
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-05-16 06:34:18 +0800

    v10.8 Navbar Navigation Optimization

--- src/components/navbar/AuthNav.tsx
+++ src/components/navbar/AuthNav.tsx
@@ -1,13 +1,17 @@
 //Navigator Components
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function AuthNav() {
+	const navigate = useNavigate();
 	return (
 		<>
 			<div className='nav'>
-				<div className='nav-col1'>
+				<div className='nav-col1' onClick={() => navigate('/')}>
 					<h1 className='nav-col1-text'>AltruWiz</h1>
-					<img src='/assets/altruwiz-logo-colored.svg' className='nav-col1-icon' />
+					<img
+						src='/assets/altruwiz-logo-colored.svg'
+						className='nav-col1-icon'
+					/>
 				</div>
 			</div>
 		</>
--- src/components/navbar/LandingNav.tsx
+++ src/components/navbar/LandingNav.tsx
@@ -1,11 +1,12 @@
 //Navigator Components
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function LandingNav() {
+	const navigate = useNavigate();
 	return (
 		<>
 			<div className='nav'>
-				<div className='nav-col1'>
+				<div className='nav-col1' onClick={() => navigate('/')}>
 					<h1 className='nav-col1-text'>AltruWiz</h1>
 					<img
 						src='/assets/altruwiz-logo-colored.svg'
