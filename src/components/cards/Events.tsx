370038cbc0a1349957fdd2b87aa3b3c7da849fe2
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-05-26 17:44:10 +0800

    v11.8 Optimized Search UI

--- src/components/cards/Events.tsx
+++ src/components/cards/Events.tsx
@@ -9,18 +9,21 @@ function Events() {
 	return (
 		<div className='event'>
 			<div className='event-body'>
-				<TextField
-					variant='outlined'
-					color='secondary'
-					size='small'
-					className='signin-body-container-col2-form-fields-field'
-					margin='dense'
-					placeholder='Search Anything'
-					value={search}
-					onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-						setSearch(event.target.value)
-					}
-				/>
+				<div className='event-body-searchbar'>
+					<TextField
+						variant='outlined'
+						color='secondary'
+						size='small'
+						className='event-body-searchbar-field'
+						margin='dense'
+						placeholder='Search Anything'
+						value={search}
+						onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+							setSearch(event.target.value)
+						}
+					/>
+				</div>
+
 				{!search && (
 					<>
 						<EventList
