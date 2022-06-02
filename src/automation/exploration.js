0d6fd17d44ba5972c7e22a0757e49a3796f4a2cb
CyberBlaze007 <cyberblazeflo7@gmail.com>
2022-06-02 13:26:25 +0800

    Incomplete Rules and Automate edit
    -Init of proper segmentation of access to firestore rules
    -Minor change to link of Altruwiz

--- src/automation/exploration.js
+++ src/automation/exploration.js
@@ -1,3 +1,40 @@
+// const { chromium } = require('playwright');
+// (async () => {
+//   const browser = await chromium.launch({
+//     headless: false
+//   });
+//   const context = await browser.newContext();
+//   const page = await context.newPage();
+  
+//   // await page.goto('http://localhost:3000/'); //replace with live url
+//   await page.goto('https://altruwiz.web.app/');
+
+//   await page.locator('text=Find your next event').click({delay: 2000});
+//   page.once('dialog', dialog => {
+//     console.log(`Dialog message: ${dialog.message()}`);
+//     dialog.dismiss().catch(() => {});
+//   });
+//   await page.locator('text=Dashboard').click({delay: 2000});
+//   await page.locator('input[type="text"]').click({delay: 2000});
+//   await page.locator('input[type="text"]').fill('testcinder00000001@gmail.com');
+//   await page.locator('input[type="password"]').click({delay: 2000});
+//   await page.locator('input[type="password"]').fill('poiuytrewq');
+//   await Promise.all([
+//     // page.waitForNavigation(/*{ url: 'http://localhost:3000/dashboard/profile' }*/),
+//     page.waitForNavigation(/*{ url: 'https://altruwiz.web.app/dashboard/profile' }*/),
+//     page.locator('button:has-text("Login")').click()
+//   ]);
+//   await page.locator('button[role="tab"]:has-text("Events")').click({delay: 2000});
+//   await page.locator('text=Achievements').click({delay:2000});
+//   await page.locator('text=Certificates').click({delay:2000});
+//   await page.locator('text=Badges').click({delay:2000});
+//   await page.locator('text=Achievements').click({delay:2000});
+//   await context.close();
+//   await browser.close();
+// })();
+
+
+//Rigor Test Version
 const { chromium } = require('playwright');
 (async () => {
   const browser = await chromium.launch({
@@ -5,26 +42,28 @@ const { chromium } = require('playwright');
   });
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto('http://localhost:3000/');
-  await page.locator('text=Find your next event').click({delay: 2000});
+  await page.goto('https://altruwiz.web.app/');
+  await page.locator('text=Find your next event').click();
   page.once('dialog', dialog => {
     console.log(`Dialog message: ${dialog.message()}`);
     dialog.dismiss().catch(() => {});
   });
-  await page.locator('text=Dashboard').click({delay: 2000});
-  await page.locator('input[type="text"]').click({delay: 2000});
+  await page.locator('text=Dashboard').click();
+  await page.locator('input[type="text"]').click();
   await page.locator('input[type="text"]').fill('testcinder00000001@gmail.com');
-  await page.locator('input[type="password"]').click({delay: 2000});
+  await page.locator('input[type="password"]').click();
   await page.locator('input[type="password"]').fill('poiuytrewq');
   await Promise.all([
-    page.waitForNavigation(/*{ url: 'http://localhost:3000/dashboard/profile' }*/),
+    page.waitForNavigation(/*{ url: 'https://altruwiz.web.app/dashboard/profile' }*/),
     page.locator('button:has-text("Login")').click()
   ]);
-  await page.locator('button[role="tab"]:has-text("Events")').click({delay: 2000});
-  await page.locator('text=Achievements').click({delay:2000});
-  await page.locator('text=Certificates').click({delay:2000});
-  await page.locator('text=Badges').click({delay:2000});
-  await page.locator('text=Achievements').click({delay:2000});
+  await page.locator('button[role="tab"]:has-text("Events")').click();
+  await page.locator('text=Achievements').click();
+  await page.locator('text=Certificates').click();
+  await page.locator('text=Badges').click();
+  await page.locator('text=Achievements').click();
   await context.close();
   await browser.close();
 })();
+
+// while(node exploration.js); do :; done
