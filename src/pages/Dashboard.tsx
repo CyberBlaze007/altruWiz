54530753ab7ca2fb846106eb7951e96a8f71500b
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-06-04 11:29:51 +0800

    v13.6 Conflict Resolved

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -88,11 +88,8 @@ function Dashboard() {
 
 	return (
 		<div className='dashboard'>
-<<<<<<< Updated upstream
 			{/* <Navigator /> */}
-=======
 			<Code showModal={showModal} setShowModal={setShowModal} />
->>>>>>> Stashed changes
 			<div className='dashboard-container'>
 				<div className='dashboard-container-nav'>
 					<DBNav />
@@ -100,43 +97,37 @@ function Dashboard() {
 				<TabsUnstyled
 					defaultValue={0}
 					value={index}
-					className='dashboard-container-tab'
-				>
+					className='dashboard-container-tab'>
 					{/* <ArrowBackIosIcon className='dashboard-container-control' /> */}
 					<TabsList className='dashboard-container-tab-list' id={'tablist'}>
 						<Tab
 							onClick={() => {
 								navigate('/dashboard/profile');
-							}}
-						>
+							}}>
 							Profile
 						</Tab>
 						<Tab
 							onClick={() => {
 								navigate('/dashboard/events');
-							}}
-						>
+							}}>
 							Events
 						</Tab>
 						<Tab
 							onClick={() => {
 								navigate('/dashboard/achievements');
-							}}
-						>
+							}}>
 							Achievements
 						</Tab>
 						<Tab
 							onClick={() => {
 								navigate('/dashboard/certificates');
-							}}
-						>
+							}}>
 							Certificates
 						</Tab>
 						<Tab
 							onClick={() => {
 								navigate('/dashboard/badges');
-							}}
-						>
+							}}>
 							Badges
 						</Tab>
 					</TabsList>
