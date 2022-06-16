731d8f762534d062b10613bf8906f9a9d432b087
Nash Uriel A. Tapayan <tnashuriel@gmail.com>
2022-06-16 21:22:04 +0800

    v20.9 Save Certs Function

--- src/components/modals/Certificate.tsx
+++ src/components/modals/Certificate.tsx
@@ -1,17 +1,40 @@
 import { motion } from 'framer-motion';
+import { saveAs } from 'file-saver';
 import DownloadIcon from '@mui/icons-material/Download';
+import PictureAsPdfOutlinedIcon from '@mui/icons-material/PictureAsPdfOutlined';
 import { useNavigate, useParams } from 'react-router-dom';
 import Cert from '../cert/Cert';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 function ModalCert({ showModal, setShowModal, user, event }: any) {
 	const navigate = useNavigate();
 	const { id } = useParams();
+
+	const handleSave = () => {
+		html2canvas(document.querySelector('#toSave')).then((canvas) => {
+			canvas.toBlob((blob) => {
+				saveAs(blob, `${user}.jpg`);
+			});
+		});
+	};
+
+	const handlePdf = () => {
+		html2canvas(document.querySelector('#toSave')).then((canvas) => {
+			var myImage = canvas.toDataURL('image/jpeg,1.0');
+			// Adjust width and height
+			var imgWidth = 280;
+			var imgHeight = 200;
+			// jspdf changes
+			var pdf = new jsPDF('l', 'mm', 'a4');
+			pdf.addImage(myImage, 'jpeg', 8.5, 5, imgWidth, imgHeight); // 2: 19
+			pdf.save(`${user}.pdf`);
+		});
+	};
+
 	return (
 		<motion.div
 			className='certificates-modal'
-			onClick={() => {
-				setShowModal(false), navigate(-1);
-			}}
 			initial={{
 				scale: 0,
 				opacity: 0,
@@ -28,6 +51,12 @@ function ModalCert({ showModal, setShowModal, user, event }: any) {
 							opacity: { delay: 0.3, duration: 0.3, type: 'tween' },
 					  }
 			}>
+			<div
+				onClick={() => {
+					setShowModal(false), navigate(-1);
+				}}
+				className='certificates-modal-void'
+			/>
 			<motion.div
 				initial={{
 					scale: 0,
@@ -43,37 +72,47 @@ function ModalCert({ showModal, setShowModal, user, event }: any) {
 						  }
 				}
 				className='certificates-modal-container'>
-				<Cert
-					name={user}
-					title={
-						showModal
-							? event.at(
-									event.findIndex((data) => {
-										return id === data.eventID;
-									}),
-							  ).eventName
-							: ''
-					}
-					org={
-						showModal
-							? event.at(
-									event.findIndex((data) => {
-										return id === data.eventID;
-									}),
-							  ).eventCreator
-							: ''
-					}
-					date={
-						showModal
-							? event.at(
-									event.findIndex((data) => {
-										return id === data.eventID;
-									}),
-							  ).eventDate
-							: ''
-					}
-				/>
-				<DownloadIcon />
+				<div id='toSave' className='certificates-modal-container-cert'>
+					<Cert
+						name={user}
+						title={
+							showModal
+								? event.at(
+										event.findIndex((data) => {
+											return id === data.eventID;
+										}),
+								  ).eventName
+								: ''
+						}
+						org={
+							showModal
+								? event.at(
+										event.findIndex((data) => {
+											return id === data.eventID;
+										}),
+								  ).eventCreator
+								: ''
+						}
+						date={
+							showModal
+								? event.at(
+										event.findIndex((data) => {
+											return id === data.eventID;
+										}),
+								  ).eventDate
+								: ''
+						}
+					/>
+				</div>
+
+				<div className='certificates-modal-container-downloads'>
+					<motion.div whileHover={{ y: '-2px' }} whileTap={{ y: '2px' }} onClick={handlePdf} className='certificates-modal-container-downloads-icon'>
+						<PictureAsPdfOutlinedIcon className='certificates-modal-container-downloads-icon-comp' />
+					</motion.div>
+					<motion.div whileHover={{ y: '-2px' }} whileTap={{ y: '2px' }} onClick={handleSave} className='certificates-modal-container-downloads-icon'>
+						<DownloadIcon className='certificates-modal-container-downloads-icon-comp' />
+					</motion.div>
+				</div>
 			</motion.div>
 		</motion.div>
 	);
