9dfa6ca17fe92dea11e9fed04366fdb213279a1b
ZenShi <velezrenz@gmail.com>
2022-06-16 14:42:40 +0800

    19.2 OrgDash Done

--- src/pages/OrgDash.tsx
+++ src/pages/OrgDash.tsx
@@ -9,6 +9,9 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { onSnapshot, collection, query, where } from 'firebase/firestore';
 import DataService from '../firebase/services';
 
+import Create from '../components/modals/Create';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+
 function Organization() {
 	const [orgName, setOrgName] = useState('');
 	const [orgDesc, setOrgDesc] = useState('');
@@ -43,63 +46,101 @@ function Organization() {
 	};
 
 	return (
-		<div className='organizers'>
-			<div className='organizers-navbar'>
-				<DBNav />
-			</div>
-			<div className='organizers-body'>
-				<div className='organizers-body-info'>
-					<div className='organizers-body-info-title'>
-						<h1 className='organizers-body-info-title-text'>
-							Alt-F4 Organization
-						</h1>
-
-						<Button
-							className='organizers-body-info-title-icon'
-							color='secondary'
-							startIcon={!orgDescEdit ? <CheckIcon /> : <EditOutlinedIcon />}
-							onClick={() => {
-								setOrgDescEdit(!orgDescEdit);
-								updateDesc(orgDesc);
-							}}></Button>
-					</div>
-					<div className='organizers-body-info-org'>
-						<div className='organizers-body-info-org-name'>
-							<h1 className='organizers-body-info-org-name-t'>
-								Org Information
+		<>
+			<Create showModal={showModal} setShowModal={setShowModal} />
+			<div className='organizers'>
+				<div className='organizers-navbar'>
+					<DBNav />
+				</div>
+				<div className='organizers-body'>
+					<div className='organizers-body-info'>
+						<div className='organizers-body-info-title'>
+							<h1 className='organizers-body-info-title-text'>
+								Alt-F4 Organization
 							</h1>
+
+							<Button
+								className='organizers-body-info-title-icon'
+								color='secondary'
+								startIcon={!orgDescEdit ? <CheckIcon /> : <EditOutlinedIcon />}
+								onClick={() => {
+									setOrgDescEdit(!orgDescEdit);
+									updateDesc(orgDesc);
+								}}></Button>
 						</div>
+						<div className='organizers-body-info-org'>
+							<div className='organizers-body-info-org-name'>
+								<h1 className='organizers-body-info-org-name-t'>
+									Org Information
+								</h1>
+							</div>
 
-						<div className='organizers-body-info-org-description'>
-							<h1 className='organizers-body-info-org-description-t'>
-								We are always active in closing active applications. We have
-								never failed in our mission. Our purpose is to provide
-								sustainable and productive user experience for people. We aim to
-								solve error crisis and freedom.
+							<div className='organizers-body-info-org-description'>
+								<h1 className='organizers-body-info-org-description-t'>
+									We are always active in closing active applications. We have
+									never failed in our mission. Our purpose is to provide
+									sustainable and productive user experience for people. We aim
+									to solve error crisis and freedom.
+								</h1>
+							</div>
+						</div>
+					</div>
+					<div className='organizers-body-events'>
+						<div className='organizers-body-events-title'>
+							<h1 className='organizers-body-events-title-t'>Events</h1>
+						</div>
+						<div className='organizers-body-events-table'>
+							<table className='organizers-body-events-table-component'>
+								<thead>
+									<tr>
+										<th>Event Code</th>
+										<th>Date</th>
+										<th>Name</th>
+										<th>Participants</th>
+										<th></th>
+										<th></th>
+									</tr>
+								</thead>
+								<tbody>
+									{eventsCreated.map((element) => (
+										<tr
+											key={element}
+											className='orgDashboard-events-table-component-data'>
+											<td>{element}</td>
+											<td>{element}</td>
+											<td>{element}</td>
+											<td>{element}</td>
+											<td>
+												<div className='orgDashboard-events-table-component-data-icons'>
+													<EditOutlinedIcon className='orgDashboard-events-table-component-data-icons-ic' />
+												</div>
+											</td>
+											<td>
+												<div className='orgDashboard-events-table-component-data-icons'>
+													<DeleteOutlineIcon className='orgDashboard-events-table-component-data-icons-ic' />
+												</div>
+											</td>
+										</tr>
+									))}
+								</tbody>
+							</table>
+						</div>
+						<div className='organizers-body-events-create'>
+							<h1 className='organizers-body-events-create-text'>
+								Create New Event
 							</h1>
+
+							<Button
+								className='organizers-body-events-create-icon'
+								endIcon={<AddOutlinedIcon />}
+								onClick={() => {
+									setShowModal(true);
+								}}></Button>
 						</div>
 					</div>
 				</div>
-				<div className='organizers-body-events'>
-					<div className='organizers-body-events-title'>
-						<h1 className='organizers-body-events-title-t'>Events</h1>
-					</div>
-					<div className='organizers-body-events-table'></div>
-					<div className='organizers-body-events-create'>
-						<h1 className='organizers-body-events-create-text'>
-							Create New Event
-						</h1>
-
-						<Button
-							className='organizers-body-events-create-icon'
-							endIcon={<AddOutlinedIcon />}
-							onClick={() => {
-								setShowModal(true);
-							}}></Button>
-					</div>
-				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
